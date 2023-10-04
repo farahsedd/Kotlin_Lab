4d2c61651cb965f6027879563be9ec906f4c49f1
farah seddik <farahseddik500@gmail.com>
2023-10-04 19:04:07 +0100

    first commit

---
+++ tp0/app/src/main/java/com/gl4/tp/Hello.kt
@@ -0,0 +1,32 @@
+package com.gl4.tp
+
+
+fun main(args: Array<String>) {
+    /*var hello = "Hello"
+    hello = "Hello world!"
+
+    println(hello)
+
+    var toto = "Toto"
+
+    println(toto)
+
+    var message: String? = "I’m learning Kotlin!"
+   // message = null
+    println(message.toString())*/
+
+
+    /*fun sum(a:Int,b:Int):Int{
+        return a+b
+    }*/
+
+    /*val name="chiraz"
+    fun sayMyName() { println(" $name ") }*/
+
+    /*fun sayHello(): String = "Hello"*/
+
+
+
+
+
+}
